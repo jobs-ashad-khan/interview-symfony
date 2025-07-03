097e830d0e4c28ea7bd8929684b63c9d654e35c3
Antoine Aresu <aresu.antoine@gmail.com>
2025-07-03 13:33:42 +0200

    :arrow_up: [Composer] Upgrade a few

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -17,7 +17,7 @@ class UserFixtures extends Fixture
         $this->hasher = $hasher;
     }
 
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $user = (new User())->setEmail('tester@gmail.com');
         $manager->persist($user);
