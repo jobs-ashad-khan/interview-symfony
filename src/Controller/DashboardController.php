252d42965ec4e5b5e17e8f9311bb9f01ea030755
Khan Ashad <jobs.ashad.khan@gmail.com>
2025-07-09 16:30:55 +0200

    Rename HomeController to DashboardController

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,20 +7,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class DashboardController extends AbstractController
 {
     public function __construct(
         private readonly BeneficiaryGeneratorService $beneficiaryGeneratorService
     ) {
     }
 
-    #[Route('/', name: 'app_home')]
-    public function home(): Response
+    #[Route('/', name: 'app_dashboard')]
+    public function dashboard(): Response
     {
         $beneficiaries = $this->beneficiaryGeneratorService->getRandomBeneficiaries(12);
 
-        return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('dashboard/dashboard.html.twig', [
+            'controller_name' => 'DashboardController',
             'beneficiaries' => $beneficiaries,
         ]);
     }
